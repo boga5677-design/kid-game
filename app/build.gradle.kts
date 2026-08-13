5e96f81857daddff4326afb5ea6ceb4bc119cb1b
boga5677-design <boga5677@gmail.com>
2026-08-13 15:16:51 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,17 @@ android {
         versionName = "0.1.0"
     }
 
-    buildFeatures {
-        viewBinding = false
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
         jvmTarget = "17"
+    }
+
+    buildFeatures {
+        viewBinding = false
     }
 }
 
